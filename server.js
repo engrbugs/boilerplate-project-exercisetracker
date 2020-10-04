2fcdec5ef33b72667b87fbe36d78603cba3e0755
engrbugs <engrbugs@gmail.com>
2020-10-03 22:14:24 -0700

    last bug

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get("/api/exercise/users", (req, res) => {
 app.post("/api/exercise/add", (req, res) => {
   const { userId, description, duration, date } = req.body;
 
-  const dateObj = date === '' ? new Date().toISOString().toUTCString().substring(0, 16).replace(/,/, "") : new Date(date).toUTCString().substring(0, 16).replace(/,/, "");
+  const dateObj = date === '' ? new Date().toUTCString().substring(0, 16).replace(/,/, "") : new Date(date).toUTCString().substring(0, 16).replace(/,/, "");
   
   console.log('ADD:', userId, description, duration, date);
   // console.log(dateObj);
